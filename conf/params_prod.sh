f6ead1bf101d696a46af7f3e31223d7c42ca52ed
ronelis <ronald.ekowellis@gmail.com>
2023-09-25 10:40:18 -0400

    Update params_prod.sh

--- conf/params_prod.sh
+++ conf/params_prod.sh
@@ -1,13 +1,13 @@
 # Stack parameter
-export ORG_ACCOUNT_ID='' # ID for Organization Management account 
+export ORG_ACCOUNT_ID='151602076181' # ID for Organization Management account 
 export ORG_ROLE=OrganizationsReadOnlyAccess
-export AWS_REGION=eu-west-1
+export AWS_REGION=us-east-1
 export EXTERNAL_ID='' #Optional 
-export JIRA_DEFAULT_ASSIGNEE='' #ID for default assignee for all Security Issues
-export JIRA_INSTANCE="" #HTTPS address for JIRA server (exclude schema "https://")
-export JIRA_PROJECT_KEY="" # JIRA Project Key
-export ISSUE_TYPE="" #JIRA Issuetype name: Example, "Bug", "Security Issue"
-export REGIONS=("eu-west-1") # List of regions deployed
+export JIRA_DEFAULT_ASSIGNEE='712020%3Aed944807-0059-4e42-bfc3-667afa429916' #ID for default assignee for all Security Issues
+export JIRA_INSTANCE="ronaldjira.atlassian.net" #HTTPS address for JIRA server (exclude schema "https://")
+export JIRA_PROJECT_KEY="SEC" # JIRA Project Key
+export ISSUE_TYPE="Bug" #JIRA Issuetype name: Example, "Bug", "Security Issue"
+export REGIONS=("us-east-1") # List of regions deployed
 
 PARAMETERS=(
   "OrganizationManagementAccountId=$ORG_ACCOUNT_ID"
